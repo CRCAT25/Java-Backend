29b14f3e4e6c79092e85353c9ea6b4f1a6908fe6
thanhdev <doquocthanh@github.com>
2024-12-07 01:30:36 +0700

    Initial commit

---
+++ src/main/java/com/rs/retailstore/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.rs.retailstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+//        Dùng để encode password thay vì tải Spring Boot CLI
+//        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+//        System.out.println("Hashed Password: " + hashedPassword);
+
+        // Tạo user mới là user
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$10$jauzj7Dap4207RyRPQT7NeXXCpxHt8d3gVSu.JXUxzI.Rxtt50RUy")
+                .roles("USER")
+                .build();
+
+        // Tạo user mới là admin
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$WHhFJ0VIwMc86aaIiG4uHuReoDsCMlBA5zlTBn9MBT85E30RnIKD2")
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
