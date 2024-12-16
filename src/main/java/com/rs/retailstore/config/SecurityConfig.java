0865e7439866caf58872493db0f6433892351e17
thanhdev <doquocthanh@github.com>
2024-12-16 23:48:05 +0700

    Add: Connect to DB and create users, authorizations

--- src/main/java/com/rs/retailstore/config/SecurityConfig.java
+++ src/main/java/com/rs/retailstore/config/SecurityConfig.java
@@ -6,31 +6,44 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(DataSource dataSource) {
 //        Dùng để encode password thay vì tải Spring Boot CLI
-//        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
-//        System.out.println("Hashed Password: " + hashedPassword);
+        String hashedPassword = BCrypt.hashpw("12345", BCrypt.gensalt());
+        System.out.println("Hashed Password: " + hashedPassword);
 
         // Tạo user mới là "user"
         UserDetails user = User.builder()
                 .username("user")
-                .password("{bcrypt}$2a$10$jauzj7Dap4207RyRPQT7NeXXCpxHt8d3gVSu.JXUxzI.Rxtt50RUy")
+                .password("$2a$10$UsqFfE2UvnAgNgWAqQnaxO54EiqpyLehBEHR3MuVMtkCzBdcTNeLS")
                 .roles("USER")
                 .build();
 
         // Tạo user mới là "admin"
         UserDetails admin = User.builder()
                 .username("admin")
-                .password("{bcrypt}$2a$10$WHhFJ0VIwMc86aaIiG4uHuReoDsCMlBA5zlTBn9MBT85E30RnIKD2")
+                .password("$2a$10$UsqFfE2UvnAgNgWAqQnaxO54EiqpyLehBEHR3MuVMtkCzBdcTNeLS")
                 .roles("USER", "ADMIN")
                 .build();
 
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
